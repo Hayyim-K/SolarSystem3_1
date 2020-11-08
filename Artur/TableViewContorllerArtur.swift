727f1625a1e1b0b49ee85f04e91c29d856329c56
Artur <Gedakyan>
2020-11-08 14:03:48 +0300

    v2.0

--- Artur/TableViewContorllerArtur.swift
+++ Artur/TableViewContorllerArtur.swift
@@ -9,8 +9,7 @@ import UIKit
 
 class TableViewControllerArtur: UITableViewController {
     
-    var planetList = Planet.getPlanets
-    var planet2 = DataManager()
+    var planetList = Planet.getPlanets()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +21,42 @@ class TableViewControllerArtur: UITableViewController {
 }
 
 extension TableViewControllerArtur {
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        planet2.names.count
+        planetList.count
     }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "planetName", for: indexPath)
+        
+        let planet = planetList[indexPath.row]
+        cell.textLabel?.text = planet.name
+        cell.textLabel?.numberOfLines = 0
+        //          cell.detailTextLabel?.text = planet.distanceToSun
+        cell.imageView?.image = UIImage(named: planet.name)
+        //
+        return cell
+    }
+}
+
+extension TableViewControllerArtur {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let planet = planetList[indexPath.row]
+        performSegue(withIdentifier: "showDetails", sender: planet)
+    }
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        .none
+    }
+    
+    override func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+        false
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let currentPlanet = planetList.remove(at: sourceIndexPath.row)
+        planetList.insert(currentPlanet, at: destinationIndexPath.row)
+    }
+    
 }
