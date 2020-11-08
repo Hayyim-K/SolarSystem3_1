ed84debb45b936ccc01f4892dd17e30b549cd632
Vladislav <vladislav.y.popov@gmail.com>
2020-11-08 14:49:05 +0300

    Добавлена фоновая картинка - nightSky Созданы 3 viewController и 1 TabBarController.

---
+++ SolarSystem3/PlanetsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PlanetsViewController.swift
+//  SolarSystem3
+//
+//  Created by Vladislav on 08.11.2020.
+//
+
+import UIKit
+
+class PlanetsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+   
+
+}
---
+++ SolarSystem3/SolarSystemViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SolarSystemViewController.swift
+//  SolarSystem3
+//
+//  Created by Vladislav on 08.11.2020.
+//
+
+import UIKit
+
+class SolarSystemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
